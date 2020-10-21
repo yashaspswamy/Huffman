445d998cf8552ce73682b6f6cf6b78e8ec3474a3
Sathish A <sathish97a@gmail.com>
2020-10-21 16:49:49 +0530

    Add profile

--- profile.sh
+++ profile.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # profile encode
 echo "Encoding"
-perf stat -e cycles,instructions,cache-references,cache-misses,branches,branch-misses,task-clock,faults,minor-faults,cs,migrations python3 huffman.py -e story.txt -o story.huff
+sudo perf stat -e cycles,instructions,cache-references,cache-misses,branches,branch-misses,task-clock,faults,minor-faults,cs,migrations python3 huffman.py -e story.txt -o story.huff
 # profile decode
 echo "Decoding"
-perf stat -e cycles,instructions,cache-references,cache-misses,branches,branch-misses,task-clock,faults,minor-faults,cs,migrations python3 huffman.py -d story.huff -o story_.txt
+sudo perf stat -e cycles,instructions,cache-references,cache-misses,branches,branch-misses,task-clock,faults,minor-faults,cs,migrations python3 huffman.py -d story.huff -o story_.txt
