7eacba7eb58b2dca1e74e6e76fbc9a1dcb65a844
Sathish A <sathish97a@gmail.com>
2020-10-21 14:23:11 +0530

    Add compression ratio

--- run.sh
+++ run.sh
@@ -5,3 +5,6 @@ time python3 huffman.py -e story.txt -o story.huff
 time python3 huffman.py -d story.huff -o story_.txt
 # find differences
 ./diff.sh story.txt story_.txt
+#find compression ratio
+echo "Compression ratio"
+expr $(ls -l story.txt | awk '{ print $5 }')/$(ls -l story.huff | awk '{ print $5 }') | bc -l
